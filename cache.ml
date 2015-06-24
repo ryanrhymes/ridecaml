e3d18f2eafb37722454e1315999c1090fa760260
Liang Wang <ryanrhymes@gmail.com>
2015-06-24 22:54:41 +0100

    coding ...

--- cache.ml
+++ cache.ml
@@ -1,11 +1,15 @@
 (**
-  Liang Wang @ Computer Lab, Cambridge University, UK
-  2015.06.24
+   Cache module for service caching.
+
+   Liang Wang @ Computer Lab, Cambridge University, UK
+   2015.06.24
 **)
 
 type service = {
-  id : string;
-  size : int
+  id : int;
+  uri : string;
+  timestamp : float;
+  size : int;
 }
 
 let service_cache = Hashtbl.create 1024;;
@@ -15,3 +19,14 @@ let add s = Hashtbl.add service_cache s.id s;;
 let remove sid = Hashtbl.remove service_cache sid;;
 
 let find sid = Hashtbl.find service_cache sid;;
+
+let touch sid = 
+  let s = find sid in
+  s.timestamp = Unix.gettimeofday ();;
+
+let print sid = 
+  let s = find sid in
+  Printf.printf "{ id = %i; uri = %s; time = %f size = %i }\n" s.id s.uri s.timestamp s.size;;
+
+let debug c = Hashtbl.iter (fun k v -> print k) c;;
+
---
+++ test_cache.ml
@@ -0,0 +1,25 @@
+(**
+   Test the cache module.
+
+   Liang Wang @ Computer Lab, Cambridge University, UK
+   2015.06.24
+**)
+
+print_endline "Testing cache module --> 1 ..."
+let s1 : Cache.service = {id = 1; uri = "wwww.google.com"; timestamp = 0.; size = 12345;};;
+let s2 : Cache.service = {id = 2; uri = "wwww.yahoo.com"; timestamp = 0.; size = 78901;};;
+Cache.add s1;;
+Cache.add s2;;
+Cache.debug Cache.service_cache;;
+
+print_endline "Test caching module --> 2 ...";;
+for i = 3 to 30 do
+  let s : Cache.service = {id = i; uri = "my_uri_" ^ string_of_int i; timestamp = 0.1; size = i * 10;} in
+  Cache.add s;
+  Cache.touch s.id
+done;;
+Cache.debug Cache.service_cache;;
+
+print_endline "Test caching module --> 3 ...";;
+Cache.print 1;;
+Cache.print 2;;
