c9891c0b19b593b1075e432790ed4e1d2a1a78e9
Liang Wang <ryanrhymes@gmail.com>
2015-06-25 14:47:45 +0100

    coding ...

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: test
 
 test: docker.ml
-	ocamlbuild -r -use-ocamlfind -package cohttp docker.native
+	ocamlbuild -r -use-ocamlfind -package lwt,cohttp,cohttp.lwt docker.native
 
 clean:
 	rm -fr _build *.native *.cmi *.cmo *.annot *~
--- docker.ml
+++ docker.ml
@@ -5,7 +5,23 @@
    2015.06.25
 **)
 
+open Lwt
 open Cohttp
+open Cohttp_lwt_unix
+
+let d_uri = "128.232.65.27"
+
+let body =
+  Client.get (Uri.of_string "http://www.reddit.com/") >>= fun (resp, body) ->
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  (**Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);**)
+  body |> Cohttp_lwt_body.to_string >|= fun body ->
+  Printf.printf "Body of length: %d\n" (String.length body);
+  body
 
 let () =
-  print_endline "hello"
+  print_endline ("Docker OCaml API @ " ^ d_uri);;
+  let body = Lwt_main.run body in
+  print_endline ("Received body\n" ^ body)
+
