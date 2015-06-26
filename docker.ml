88fb0419677ab74bb15ffae2935e4ad6a7a6eb3c
Liang Wang <ryanrhymes@gmail.com>
2015-06-26 01:40:15 +0100

    test ...

--- docker.ml
+++ docker.ml
@@ -11,16 +11,11 @@ open Cohttp_lwt_unix
 
 let d_uri = "http://128.232.65.27:2375/images/json"
 
-let test =
+let docker_deamon =
   Client.get (Uri.of_string d_uri) >>= fun (resp, body) ->
-  let code = resp |> Response.status |> Code.code_of_status in
-  Printf.printf "Response code: %d\n" code;
-  (**Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);**)
-  body |> Cohttp_lwt_body.to_string >|= fun body ->
-  Printf.printf "Body of length: %d\n" (String.length body);
-  body
+  body |> Cohttp_lwt_body.to_string
 
 let () =
   print_endline ("Docker OCaml API @ " ^ d_uri);;
-  let x = Lwt_main.run test in
+  let x = Lwt_main.run docker_deamon in
   print_endline ("Received body\n" ^ x)
