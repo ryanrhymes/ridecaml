6df7225e84ba6a6cfb892f0eba28c80ceeca0f83
Liang Wang <ryanrhymes@gmail.com>
2015-07-06 01:27:21 +0100

    coding ...

--- docker.ml
+++ docker.ml
@@ -14,12 +14,16 @@ open Cohttp_lwt_unix
 let show_headers h =
   Cohttp.Header.iter (fun k v -> List.iter (Printf.eprintf "%s: %s\n%!" k) v) h
 
+let test2 s = 
+  print_endline s;
+  print_endline "=======>"
+
 let test1 uri = 
   let headers = Cohttp.Header.of_list [] in
   let s = Client.get (Uri.of_string uri) >>= fun (res, body) ->
     let headers = Response.headers res in
     show_headers headers;
-    Lwt_stream.iter_s (fun s -> return ()) (Cohttp_lwt_body.to_stream body)
+    Lwt_stream.iter_s (fun s -> test2 s; return ()) (Cohttp_lwt_body.to_stream body)
   in Lwt_main.run s
 (**  let s = Client.call ~headers `GET (Uri.of_string uri) >>= fun (res, body) ->
   Lwt_stream.iter_s (fun s -> print_endline s; return ()) (Cohttp_lwt_body.to_stream body)
