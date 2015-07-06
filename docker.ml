98c77fa933814586d5ee204607b4895e522f059e
Liang Wang <ryanrhymes@gmail.com>
2015-07-06 01:37:30 +0100

    coding ...

--- docker.ml
+++ docker.ml
@@ -25,10 +25,7 @@ let test1 uri =
     show_headers headers;
     Lwt_stream.iter_s (fun s -> test2 s; return ()) (Cohttp_lwt_body.to_stream body)
   in Lwt_main.run s
-(**  let s = Client.call ~headers `GET (Uri.of_string uri) >>= fun (res, body) ->
-  Lwt_stream.iter_s (fun s -> print_endline s; return ()) (Cohttp_lwt_body.to_stream body)
-  in Lwt_main.run s
-**)
+
 
 
 (** These are common functions. **)
